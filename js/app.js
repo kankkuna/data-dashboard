7d448e5ad4d6dfd0e82c56412ab8e4bd3874b484
kankkuna <karina842@hotmail.com>
2017-11-29 13:00:02 -0500

    avance click en botones

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
 
-window.onload = function() {
+window.addEventListener('load', function() {
+  /*var pag = document.getElementsByTagName('globe');
+  var divsecond = document.getElementsByTagName('info');
+  pag.classList.add('divesecond');
+  cli.innerHTML = 'New Heading';*/
 
-};
+  var boton = document.getElementById('cono');
+  boton.addEventListener('click', function(event) {
+    event.preventDefault();
+    var olddiv = document.getElementById('principal');
+    var newdiv = document.getElementById('two');
+    olddiv.classList.add('ocultar');
+    newdiv.classList.add('mostrar');
+    /*papa.appendChild('newdiv');
+    NECESITO QUE LOS DIV SE INTERCAMBIEN*/
+    
+  });
+});
